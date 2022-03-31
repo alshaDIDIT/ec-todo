211444b1211adc459f70c5cad4e555c34b05d24b
Youssef <youssef.as@gmail.com>
2022-03-31 18:36:58 +0200

    simple logic done

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -4,16 +4,41 @@ let ass1 = new Assignment("Laundry", false, new Date(), null);
 let ass2 = new Assignment("Cleaning", false, new Date(), null);
 
 let toDos: Assignment[] = [ass1, ass2];
+let toDosD: Assignment[] = [];
 
-let toDoList = document.getElementById("todo-list");
+let toDoList = document.getElementById("todo-list"); // can't cast HTMLUListElement
 
 for (let i = 0; i < toDos.length; i++) {
     const toDo = toDos[i];
+    const done: string = "DONE";
 
     let li = document.createElement("li");
-    li.innerHTML = toDo.title + " (" + toDo.addedAt + ")";
+    li.setAttribute("id", "l" + i.toString());
 
-    let liButton = document.createElement("button");
+    let liButton: HTMLButtonElement = document.createElement("button");
+    liButton.setAttribute("id", "b" + i.toString());
+    liButton.setAttribute("onClick", "assignmentDone()");
+    liButton.innerHTML = done;
+
+    liButton.onclick = function() {
+        if(toDo.done == false) {
+            toDo.done = true;
+            li.style.textDecoration = "line-through";
+            li.style.color = "green";
+            toDo.doneAt = new Date();
+            toDosD.push(toDo);
+        } else {
+            toDo.done = false;
+            li.style.textDecoration = "none";
+            li.style.color = "black";
+            toDo.doneAt = null;
+        }
+
+        console.log(toDo.title + " = " + done + " (" + toDo.done + ") " + toDo.doneAt);
+    };
+
+
+    li.innerHTML = toDo.title + " / " + " (" + toDo.addedAt + ")";
 
     toDoList.appendChild(li);
     toDoList.appendChild(liButton);
