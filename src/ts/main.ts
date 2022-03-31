81f868027109432ea2e2539bb904d544661fce7f
Youssef <youssef.as@gmail.com>
2022-03-31 23:05:20 +0200

    switched to div instead of ul

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -4,19 +4,66 @@ let ass1 = new Assignment("Laundry", false, new Date(), null);
 let ass2 = new Assignment("Cleaning", false, new Date(), null);
 
 let toDos: Assignment[] = [ass1, ass2];
-let toDosD: Assignment[] = [];
 
-let toDoList = document.getElementById("todo-list"); // can't cast HTMLUListElement
+let toDoList: HTMLUListElement = document.getElementById("todo-list") as HTMLUListElement;
+let toDoLists: HTMLDivElement = document.getElementById("todo-list") as HTMLDivElement;
 
 for (let i = 0; i < toDos.length; i++) {
     const toDo = toDos[i];
     const done: string = "DONE";
+    const undone: string = "UNDO";
 
-    let li = document.createElement("li");
-    li.setAttribute("id", "l" + i.toString());
+    let listObject = document.createElement("div");
+    listObject.setAttribute("id", "l" + i.toString());
+    listObject.setAttribute("class", "d-flex list-group-item justify-content-between");
+
+    let title: HTMLHeadElement = document.createElement("h3");
+    title.innerText = toDo.title;
 
     let liButton: HTMLButtonElement = document.createElement("button");
     liButton.setAttribute("id", "b" + i.toString());
+    liButton.setAttribute("class", "btn btn-primary");
+    liButton.innerHTML = done;
+
+    liButton.onclick = function() {
+        if(toDo.done == false) {
+            toDo.done = true;
+            title.style.textDecoration = "line-through";
+            liButton.setAttribute("class", "btn btn-dark");
+            liButton.innerHTML = undone;
+            title.style.color = "green";
+            toDo.doneAt = new Date();
+        } else {
+            toDo.done = false;
+            title.style.textDecoration = "none";
+            title.style.color = "black";
+            liButton.setAttribute("class", "btn btn-primary");
+            liButton.innerHTML = done;
+            toDo.doneAt = new Date();
+        }
+    };
+    
+    listObject.appendChild(title);
+    listObject.appendChild(liButton);
+
+    toDoLists.appendChild(listObject);
+}
+
+/* for (let i = 0; i < toDos.length; i++) {
+    const toDo = toDos[i];
+    const done: string = "DONE";
+    const undone: string = "UNDO";
+
+    let li = document.createElement("li");
+    li.setAttribute("id", "l" + i.toString());
+    li.setAttribute("class", "list-group-item");
+
+    let title: HTMLHeadElement = document.createElement("h3");
+    title.innerHTML = toDo.title;
+
+    let liButton: HTMLButtonElement = document.createElement("button");
+    liButton.setAttribute("id", "b" + i.toString());
+    liButton.setAttribute("class", "btn btn-primary");
     liButton.setAttribute("onClick", "assignmentDone()");
     liButton.innerHTML = done;
 
@@ -24,22 +71,25 @@ for (let i = 0; i < toDos.length; i++) {
         if(toDo.done == false) {
             toDo.done = true;
             li.style.textDecoration = "line-through";
+            liButton.setAttribute("class", "btn btn-dark");
+            liButton.innerHTML = undone;
             li.style.color = "green";
             toDo.doneAt = new Date();
-            toDosD.push(toDo);
+            li.innerHTML += " (completed: " + toDo.doneAt.toISOString().split('T')[0] + ")";
         } else {
             toDo.done = false;
             li.style.textDecoration = "none";
             li.style.color = "black";
-            toDo.doneAt = null;
+            liButton.setAttribute("class", "btn btn-primary");
+            liButton.innerHTML = done;
+            toDo.doneAt = new Date();
+            li.innerHTML = title + " (added: " + toDo.addedAt.toISOString().split('T')[0] + ")";
         }
-
-        console.log(toDo.title + " = " + done + " (" + toDo.done + ") " + toDo.doneAt);
     };
 
 
-    li.innerHTML = toDo.title + " / " + " (" + toDo.addedAt + ")";
+    li.innerHTML = title + " (added: " + toDo.addedAt.toISOString().split('T')[0] + ")";
 
-    toDoList.appendChild(li);
-    toDoList.appendChild(liButton);
-}
+    toDoList.append(li);
+    toDoList.append(liButton);
+} */
