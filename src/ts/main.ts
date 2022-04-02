eecaf28ea2b1668a1f51705abd0d63e92602a036
Youssef <youssef.as@gmail.com>
2022-04-02 02:06:53 +0200

    EC TODO IS DONE!

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,19 +1,19 @@
 import { Assignment } from "./models/Assignment";
 
-let assignment1 = new Assignment("JavaScript", false, new Date(), null);
-let assignment2 = new Assignment("Avancerad JavaScript", false, new Date(), null);
-let assignment3 = new Assignment("HTML & CSS", true, new Date(), null);
-let assignment4 = new Assignment("Praktisk databasdesign", true, new Date(), null);
-let assignment5 = new Assignment("Mobilutveckling med Java", false, new Date(), null);
-let assignment6 = new Assignment("Java web-services", true, new Date(), null);
+let assignment1 = new Assignment("JavaScript", true, new Date("2022-03-22"), new Date("2022-04-01"));
+let assignment2 = new Assignment("Avancerad JavaScript", false, new Date("2022-04-22"), null);
+let assignment3 = new Assignment("HTML & CSS", true, new Date("2022-02-22"), new Date("2022-03-01"));
+let assignment4 = new Assignment("Praktisk databasdesign", true, new Date("2022-01-22"), new Date("2022-02-01"));
+let assignment5 = new Assignment("Mobilutveckling med Java", false, new Date("2022-05-22"), null);
+let assignment6 = new Assignment("Java web-services", true, new Date("2021-12-22"), new Date("2022-01-01"));
 
 let toDos: Assignment[] = [
-    assignment1,
     assignment2,
     assignment5
 ];
 
 let toDones: Assignment[] = [
+    assignment1,
     assignment3,
     assignment4,
     assignment6
@@ -34,12 +34,15 @@ let orderADone: HTMLElement = document.getElementById("order-a-done") as HTMLEle
 let orderNDone: HTMLElement = document.getElementById("order-n-done") as HTMLElement;
 
 /*
-            INIT CALL FOR LIST
+            INIT CALL FOR LISTS
 */
 for (let i = 0; i < toDos.length; i++) {
     const assignment = toDos[i];
-    const doneAssignment = toDones[i];
     createListObject(assignment);
+}
+
+for (let i = 0; i < toDones.length; i++) {
+    const doneAssignment = toDones[i];
     createListObject(doneAssignment);
 }
 
@@ -100,87 +103,135 @@ function doneUndoButton(aDiv: HTMLDivElement, assignment: Assignment, button: HT
         undoButton(aDiv, assignment, button);    
     };
 }
-/* DONE FUNCTION */
+
+// DONE FUNCTION
 function doneButton(aDiv: HTMLDivElement, assignment: Assignment, button: HTMLButtonElement) {
     assignment.done = true;
     button.setAttribute("class", "btn btn-dark");
     button.innerHTML = undo;
     assignment.doneAt = new Date();
     doneList.appendChild(aDiv);
-    console.log(toDos.indexOf(assignment));
     toDones.push(assignment);
     toDos.splice(toDos.indexOf(assignment), 1);
-    console.log(toDos);
-    console.log(toDones);
 }
-/* UNDO FUNCTION */
+// UNDO FUNCTION
 function undoButton(aDiv: HTMLDivElement, assignment: Assignment, button: HTMLButtonElement) {
     assignment.done = false;
     button.setAttribute("class", "btn btn-success");
     button.innerHTML = done;
     assignment.doneAt = null;
-    console.log(toDones.indexOf(assignment));
     toDoList.appendChild(aDiv);
     toDos.push(assignment);
     toDones.splice(toDones.indexOf(assignment), 1);
-    
-    console.log(toDos);
-    console.log(toDones);
 }
+
 
 /* 
             SORT
 */
-  
-function isAOrder(el,index,arr) {
-    // Return true for the first element
+
+// CHECK ALPHABETIC
+function isAlphabeticOrder(el: Assignment, index: number, arr: Assignment[]) {
     if (index === 0){
       return true;
     }
     else {
-    // Compare the value of the previous element
       return (el.title.toLowerCase() >= arr[index - 1].title.toLowerCase());
     }
 }
 
-orderAToDo.onclick = function() {
-    console.log(toDos)
+// CHECK ADDED DATE
+function isTodoOrder(el: Assignment, index: number, arr: Assignment[]) {
+    if (index === 0){
+      return true;
+    }
+    else {
+      return (el.addedAt >= arr[index - 1].addedAt);
+    }
+}
 
-    if (toDos.every(isAOrder)) {
-        toDoList.innerHTML = "";
+// CHECK COMPLETED DATE
+function isDoneOrder(el: Assignment, index: number, arr: Assignment[]) {
+    if (index === 0){
+      return true;
+    }
+    else {
+      return (el.title.toLowerCase() >= arr[index - 1].title.toLowerCase());
+    }
+}
+
+// SORT TODO ALPHABETIC
+orderAToDo.onclick = function() {
+    toDoList.innerHTML = "";
+
+    if (toDos.every(isAlphabeticOrder)) {
         toDos.sort((a, b) => b.title.localeCompare(a.title));
         for (let i = 0; i < toDos.length; i++) {
             const assignment = toDos[i];
             createListObject(assignment);
         }
-        console.log(toDos);
-        console.log(toDones);
     } else {
-        toDoList.innerHTML = "";
         toDos.sort((a, b) => a.title.localeCompare(b.title));
         for (let i = 0; i < toDos.length; i++) {
             const assignment = toDos[i];
             createListObject(assignment);
         }
     }
-    console.log(toDos.every(isAOrder));
 }
 
+// SORT TODO DATE ADDED
+orderNToDo.onclick = function() {
+    toDoList.innerHTML = "";
+
+    if (toDos.every(isTodoOrder)) {
+        toDos.sort((dateA, dateB) => dateB.addedAt.getTime() - dateA.addedAt.getTime());
+        for (let i = 0; i < toDos.length; i++) {
+            const assignment = toDos[i];
+            createListObject(assignment);
+        }
+    } else {
+        toDos.sort((dateA, dateB) => dateA.addedAt.getTime() - dateB.addedAt.getTime());
+        for (let i = 0; i < toDos.length; i++) {
+            const assignment = toDos[i];
+            createListObject(assignment);
+        }
+    }
+}
+
+// SORT DONE ALPHABETIC
 orderADone.onclick = function() {
-    if (toDones.every(isAOrder)) {
-        doneList.innerHTML = "";
+    doneList.innerHTML = "";
+
+    if (toDones.every(isAlphabeticOrder)) {
         toDones.sort((a, b) => b.title.localeCompare(a.title));
         for (let i = 0; i < toDones.length; i++) {
             const assignment = toDones[i];
             createListObject(assignment);
         }
-        return;
+    } else {
+        toDones.sort((a, b) => a.title.localeCompare(b.title));
+        for (let i = 0; i < toDones.length; i++) {
+            const assignment = toDones[i];
+            createListObject(assignment);
+        }  
     }
-    
+}
+
+// SORT DONE DATE ADDED
+orderNDone.onclick = function() {
     doneList.innerHTML = "";
-    toDones.sort((a, b) => a.title.localeCompare(b.title));
-    for (let i = 0; i < toDones.length; i++) {
-        const assignment = toDones[i];
-        createListObject(assignment);
-    }  
+
+    if (toDones.every(isTodoOrder)) {
+        toDones.sort((dateA, dateB) => dateB.doneAt.getTime() - dateA.doneAt.getTime());
+        for (let i = 0; i < toDones.length; i++) {
+            const assignment = toDones[i];
+            createListObject(assignment);
+        }
+    } else {
+        toDones.sort((dateA, dateB) => dateA.doneAt.getTime() - dateB.doneAt.getTime());
+        for (let i = 0; i < toDones.length; i++) {
+            const assignment = toDones[i];
+            createListObject(assignment);
+        }
+    }
 }
